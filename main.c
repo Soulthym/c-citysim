9f95f44324c53fa5f68b0168a57c12003a78ec0d
Soulthym <thymi92@gmail.com>
2017-10-16 16:03:02 +0200

    can now load a colored map

--- main.c
+++ main.c
@@ -1,77 +1,93 @@
 #include "main.h"
 
-typedef struct {
-	char original;
-	char special[5];
-}encod;
+const struct color COLOR = {
+    .FBLA =	"\x1b[30m",	.FRED =	"\x1b[31m",	.FGRE =	"\x1b[32m",	.FYEL =	"\x1b[33m",	.FBLU =	"\x1b[34m",	.FMAG =	"\x1b[35m",	.FCYA =	"\x1b[36m",	.FWHI =	"\x1b[37m",	.BBLA =	"\x1b[40m",	.BRED =	"\x1b[41m",	.BGRE =	"\x1b[42m",	.BYEL =	"\x1b[43m",	.BBLU =	"\x1b[44m",	.BMAG =	"\x1b[45m",	.BCYA =	"\x1b[46m",	.BWHI =	"\x1b[47m",	.RES = 	"\x1b[0m",	.BOL = 	"\x1b[1m",	.BLI = 	"\x1b[5m",	.REV = 	"\x1b[7m",	.CON = 	"\x1b[8m"};
 
-encod *ChargerEncodage(char *fichier) {
-	FILE *F = fopen(fichier, "r");
-	if (F == NULL) {
-		printf(">>%s not found\n",fichier);
-		return NULL;
-	} else {
-		printf(">>loading %s\n",fichier);
-		int n;
-		encod tmp;
-		for (n = 0; n < 256 && fscanf(F,"%c %s\n", &tmp.original, tmp.special) == 2; n++) {}
-		printf("il y a %d caracteres\n",n);
-
-		fseek(F, 0, SEEK_SET);
-		encod *Encodage = malloc(sizeof(encod) * n);
-		int i;
-		for (i = 0; i < n && fscanf(F,"%c %s\n", &Encodage[i].original, Encodage[i].special) == 2; i++) {
-			if (strcmp(Encodage[i].special,"?") == 0){
-				Encodage[i].special[0] = ' ';
-				Encodage[i].special[1] = '\0';
-			}
-			printf("%c <-> %s\n", Encodage[i].original, Encodage[i].special);
+void AffMap(map M) {
+	for (int y = 0; y < M.y; y ++) {
+		for (int x = 0; x < M.x; x++) {
+			printf("%s%s", M.map[y][x].color, M.map[y][x].disp);
 		}
-		fclose(F);
-		return Encodage;
+		printf("\n");
 	}
 }
 
-map **LoadMap(char *fichier){
+map *LoadMap(map *M, char *fichier, char *fichiercouleur){
 	FILE *F = fopen(fichier, "r");
+	FILE *FC = fopen(fichiercouleur, "r");
 	if (F == NULL) {
 		printf(">>%s not found\n", fichier);
-		return NULL;
+	} else if (FC == NULL){
+		fclose(F);
+		printf(">>%s not found\n", fichiercouleur);
 	} else {
-		int nbcol, nblin, x = 0, y = 0;
-		fscanf(F,"%d %d\n", &nbcol, &nblin);
-		map **M = malloc(sizeof(map*) * nblin);
-		for (int i = 0; i < nblin; i++) {
-			M[i] = malloc(sizeof(map) * nbcol);
+		fscanf(F,"%d %d\n", &M->x, &M->y);
+		M->map = malloc(sizeof(mape*) * M->y);
+		for (int y = 0; y < M->y; y++) {
+			M->map[y] = malloc(sizeof(mape) * M->x);
 		}
 		printf(">>loading %s\n", fichier);
-		for (char c = fgetc(F); c != EOF; c = fgetc(F), x++){
-			if (c == '\n') {
-				printf("\n");
-				x = 0;
-				y ++;
-			} else if (c & 128) {
-				M[y][x].disp = malloc(sizeof(char) * 4);
-				M[y][x].disp[0] = c;
-				M[y][x].disp[1] = fgetc(F);
-				M[y][x].disp[2] = fgetc(F);
-				M[y][x].disp[3] = '\0';
-				printf("%s", M[y][x].disp);
-			} else {
-				M[y][x].disp = malloc(sizeof(char) * 2);
-				M[y][x].disp[0] = c;
-				M[y][x].disp[1] = '\0';
-				printf("%s", M[y][x].disp);
+		char c,cc;
+		for (int y = 0; y < M->y; y++) {
+			for (int x = 0; x < M->x; x++) {
+				M->map[y][x].disp = malloc(sizeof(char) * 4);
+				c = fgetc(F);
+				cc = fgetc(FC);
+				M->map[y][x].disp[0] = c;
+				if (!(c & 128)) {
+					M->map[y][x].disp[1] = '\0';
+				} else {
+					M->map[y][x].disp[1] = fgetc(F);
+					M->map[y][x].disp[2] = fgetc(F);
+					M->map[y][x].disp[3] = '\0';
+				}
+				switch (cc) {
+					case 'K':
+						M->map[y][x].color = COLOR.FBLA;
+						break;
+					case 'R':
+						M->map[y][x].color = COLOR.FRED;
+						break;
+					case 'G':
+						M->map[y][x].color = COLOR.FGRE;
+						break;
+					case 'Y':
+						M->map[y][x].color = COLOR.FYEL;
+						break;
+					case 'B':
+						M->map[y][x].color = COLOR.FBLU;
+						break;
+					case 'M':
+						M->map[y][x].color = COLOR.FMAG;
+						break;
+					case 'C':
+						M->map[y][x].color = COLOR.FCYA;
+						break;
+					case 'W':
+						M->map[y][x].color = COLOR.FWHI;
+						break;
+					default :
+						M->map[y][x].color = COLOR.RES;
+						break;
+				}
 			}
+			fgetc(FC);
+			fgetc(F);
 		}
-		printf("\n");
-		return NULL;
+		fclose(F);
+		fclose(FC);
 	}
+	return M;
 }
 
 int main(int argc, char **argv) {
 	// ChargerEncodage("data/special_chars_encoding");
-	LoadMap("data/map_rendu");
+	map M ;
+	LoadMap(&M, "data/map_rendu","data/map_color");
+	printf("Mx = %d\nMy = %d\n", M.x, M.y);
+
+	AffMap(M);
+	printf("%s\n", COLOR.RES);
 	return 0;
 }
 
--- main.h
+++ main.h
@@ -3,11 +3,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lib/libs.h"
+
+struct color{
+	char *FBLA;
+	char *FRED;
+	char *FGRE;
+	char *FYEL;
+	char *FBLU;
+	char *FMAG;
+	char *FCYA;
+	char *FWHI;
+
+	char *BBLA;
+	char *BRED;
+	char *BGRE;
+	char *BYEL;
+	char *BBLU;
+	char *BMAG;
+	char *BCYA;
+	char *BWHI;
+
+	char *RES;
+	char *BOL;
+	char *BLI;
+	char *REV;
+	char *CON;
+};
+extern const struct color COLOR;
 
 typedef struct {
 	char *disp;
 	char *color;
+}mape;
+
+typedef struct {
+	mape **map;
+	int x;
+	int y;
 }map;
 
 #endif
